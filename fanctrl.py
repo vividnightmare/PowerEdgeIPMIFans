67420fa8910862354244ae205e4dd99698092c79
ViViD <vivnet@comcast.net>
2022-04-09 08:35:57 -0700

    comments
    added some comments so if/when i return to this i wont need to look anything up.  documentation is a good practice.

--- fanctrl.py
+++ fanctrl.py
@@ -9,6 +9,16 @@
 #   Tested on R620 and R210 II
 #   Requires ipmitool
 #
+#   ipmitool raw
+#       0x30 0x30 fan systems
+#           0x01 manual control
+#               0x00 manual
+#               0x01 auto
+#           0x02 speed control
+#               0xff all fans
+#               0x0X individual fan, where X = fan#
+#                   0x00 - 0x64 fan speed as 0-100%
+#
 ##########
 #
 
@@ -26,17 +36,19 @@ run = True
 #   our fan curve
 #   TempC : Fan%
 #####
-curve = {80 : 100,
-         75 : 75,
-         70 : 65,
-         65 : 45,
-         60 : 35,
-         55 : 30,
-         50 : 25,
-         0 : 15
-}
+curve = { 80 : 100,
+          75 : 75,
+          70 : 65,
+          65 : 45,
+          60 : 35,
+          55 : 30,
+          50 : 25,
+          0 : 15 }
 
 
+#####
+#   exit gracefully and return control, shutdown/reboot
+#####
 def handle_stop_signals(signum, frame):
     global run
     disable_manual_control()
@@ -56,6 +68,9 @@ def disable_manual_control():
     return 0
 
 
+#####
+#   fan speed for all fans in the system
+#####
 def set_fan_speed(speed):
     speed = hex(speed)
     cmd = "ipmitool raw 0x30 0x30 0x02 0xff " + speed + " 2>/dev/null"
@@ -63,6 +78,10 @@ def set_fan_speed(speed):
     return 0
 
 
+#####
+#   set fan speeds individually, numbering starts at 0 (0x00)
+#   depends on the number of fans available, unused but present 
+#####
 def set_single_fan_speed(fan, speed):
     fan = hex(fan)
     speed = hex(speed)
@@ -71,6 +90,9 @@ def set_single_fan_speed(fan, speed):
     return 0
 
 
+#####
+#   check all cpu temp senors and return the highest
+#####
 def get_cpu_temp():
     temp = psutil.sensors_temperatures()
     cpu_temp = []
@@ -81,15 +103,19 @@ def get_cpu_temp():
     return high_temp
 
 
+#####
+#   signal handling
+#####
 signal.signal(signal.SIGINT, handle_stop_signals)
 signal.signal(signal.SIGTERM, handle_stop_signals)
 
 
+#####
+#   initial startup settings
+#####
 enable_manual_control()
-
-
 fans = 15
-set_fan_speed(15)
+set_fan_speed(fans)
 
 
 while run:
